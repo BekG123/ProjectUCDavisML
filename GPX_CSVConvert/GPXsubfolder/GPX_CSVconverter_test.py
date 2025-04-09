668b313f12a6fac78d5444d4ff0f4e1976611f54
BekG123 <bekhruz.malikov@gmail.com>
2025-04-08 21:07:29 -0700

    Updates

--- GPX_CSVConvert/GPXsubfolder/GPX_CSVconverter_test.py
+++ GPX_CSVConvert/GPXsubfolder/GPX_CSVconverter_test.py
@@ -3,14 +3,14 @@ import csv
 import os
 
 #Define the file path
-file_path = ' '
+file_path = 'GraphHopper-Track-2024-08-23-2km.gpx'
 
 # Open the GPX file
 with open(file_path, 'r') as gpx_file:
     gpx = gpxpy.parse(gpx_file)
 
 # Create a CSV file
-with open('UCDavisSegundo1.csv', 'w', newline='') as csv_file:
+with open('UCDavisSegundoSocialSciMillerHall.csv', 'w', newline='') as csv_file:
     writer = csv.writer(csv_file)
     writer.writerow(['latitude', 'longitude', 'elevation', 'time'])
 
@@ -19,4 +19,3 @@ with open('UCDavisSegundo1.csv', 'w', newline='') as csv_file:
         for segment in track.segments:
             for point in segment.points:
                 writer.writerow([point.latitude, point.longitude, point.elevation, point.time])
-
